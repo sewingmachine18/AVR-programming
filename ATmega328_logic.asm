bacccdf32d3ad4d2fc5f47b83cdca244e0a6fcbf
peri-7 <pericles.alexiou@gmail.com>
2025-05-14 22:14:00 +0300

    Update ATmega328_logic.asm

--- ATmega328_logic.asm
+++ ATmega328_logic.asm
@@ -9,24 +9,24 @@
 ;F0 = (A'*B + C*D')
 ;F1 = (A+D')*(B+C') =(A*B)+(A*C')+(D'*B)+(D'C')
 ;Init PORTC as output
-    ldi r12,0b00000011 		;the 2 LSB of PORTC are for the outputs F0,F1    
-    out DDRC, r26		;make PORTC the output port
+    	ldi r12,0b00000011 	;the 2 LSB of PORTC are for the outputs F0,F1    
+    	out DDRC, r26		;make PORTC the output port
    
 ;Read A,B,C,D from the 4 LSB of PORTB 
-    clr r12
-    out DDRB,r12
-    ser r12
-    out PORTB,r12
-F0:  in r12,PINB
-	mov AN,r12 	;A's complement in LSB of AN
+    	clr r12
+    	out DDRB,r12
+    	ser r12
+    	out PORTB,r12
+F0:  	in r12,PINB
+	mov AN,r12 		;A's complement in LSB of AN
 	com AN
 	lsr r12
-	mov B,r12   	;B in LSB of B
+	mov B,r12   		;B in LSB of B
+	lsr r12	
+	mov C,r12   		;C in LSB of C
 	lsr r12
-	mov C,r12   	;C in LSB of C
-	lsr r12
-	mov DN,r12  	;D's complement in LSB of DN
+	mov DN,r12  		;D's complement in LSB of DN
 	com DN
 	lsr r12
-	and AN,B 	;AN = A'*B 
-	and C,DN 	;C = C*D'
+	and AN,B 		;AN = A'*B 
+	and C,DN 		;C = C*D'
