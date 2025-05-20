7b1bb502aeb26c911b322efacb441c340110b06c
peri-7 <pericles.alexiou@gmail.com>
2025-05-20 19:17:03 +0300

    Update ATmega328PB_lamp.c

--- c_files/ATmega328PB_lamp.c
+++ c_files/ATmega328PB_lamp.c
@@ -5,7 +5,7 @@
  * Created on May 19, 2025, 10:45 PM
  */
 
-#include <stdio.h>
+#include <stdio.h> 
 #include <stdlib.h>
 #define F_CPU 16000000UL
 #include<avr/io.h>
@@ -13,18 +13,20 @@
 #include<util/delay.h>
 
 volatile int counter = 0;
+volatile uint8_t debouncer = 0;
 
 ISR(INT1_vect) // External INT1 ISR
 {
-    if(counter!=0){
-        PORTB = 0x0F;
+    if(debouncer == 0){
+        if(counter == 0){
+            PORTB = 0x08;
+        }
+        else if(debouncer == 0){
+            PORTB = 0x0F;
+        }
+        counter = 5000;
+        debouncer = 132;
     }
-    else{
-        PORTB = 0x08;
-    }
-    
-    counter = 5000;
-   
     EIFR = (1 << INTF1); // Clear the flag of interrupt INTF1
 }
 
@@ -33,7 +35,7 @@ ISR(INT1_vect) // External INT1 ISR
  */
 int main(int argc, char** argv) {
         
-    //Interrupt on the rising edge of 
+    //Interrupt on the falling edge 
     EICRA = (1<<ISC11);
     
     //Enable interrupts
@@ -48,19 +50,17 @@ int main(int argc, char** argv) {
     
     while(1){
         PORTB = 0x00;
-        while(counter==0);
+        while(counter == 0);
         
-        while(counter>0){
-            counter-- ;
+        while(counter > 0){
+            --counter;
             _delay_ms(1);
-            if(counter==4500)
+            if(debouncer > 0)
+                --debouncer;
+            if(counter == 4500)
                 PORTB = 0x08;
         }
     }
     
-    
-    
-
-    return (EXIT_SUCCESS);
+      return (EXIT_SUCCESS);
 }
-
