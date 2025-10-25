0e95e54cb14fb3942cd227a25069c8e0347e6e8c
peri-7 <pericles.alexiou@gmail.com>
2025-10-25 18:21:23 +0300

    Create adc_avg(3_2).c

---
+++ exercise3/adc_avg(3_2).c
@@ -0,0 +1,87 @@
+
+#include <stdio.h> 
+#include <stdlib.h>
+#include<avr/io.h>
+#include<avr/interrupt.h>
+#include<util/delay.h>
+#define F_CPU 16000000UL
+
+
+int dc_value, step, counter = 0, avg = 0, temp;
+uint16_t input;
+
+uint16_t read_adc(){
+  
+    //start conversion
+    ADCSRA |= (1 << ADSC);
+    
+    //wait for conversion to complete
+    while (ADCSRA & (1 << ADSC));
+    
+    //read results
+    uint8_t low  = ADCL;
+    uint8_t high = ADCH;
+    uint16_t res = (high<<8)|low;
+    return res;
+}
+
+
+
+int main(){
+    
+    //set up timer1A
+    TCCR1B = (1<<WGM12)|(1<<CS12);
+    TCCR1A = (1<<WGM10)|(1<<COM1A1);
+    
+    //set up ports
+    DDRB = 0x2;
+    DDRD = 0xFF;
+    
+    //create table and initialize duty cycle
+    int pwd[] = {5, 20, 36, 51, 67, 82, 97, 113, 128, 143, 159, 174, 189, 205, 220, 236, 251};
+    step = 8;
+    dc_value = pwd[step];
+    OCR1AL = dc_value;
+    OCR1AH = 0;
+    
+    //set up ADC-related registers
+    ADMUX = (1<<MUX0)|(1<<REFS0);
+    ADCSRA = (1<<ADEN)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0);
+    
+    while(1){
+        
+        //check for brightness increase(PB4)
+        if((PINB & (1<<PB4)) != 0)
+            if(step < 16){
+                dc_value = pwd[++step];
+                OCR1AL = dc_value;
+            }
+        
+        //check for brightness decrease(PB5)
+        if((PINB & 1<<PB5))
+            if(step > 0){
+                dc_value = pwd[--step];
+                OCR1AL = dc_value;
+            }
+        
+        //delay and read from adc input
+        _delay_ms(100);
+        input = read_adc();
+        avg += input;
+        ++counter;
+        
+        //after 16 loops show avg through portd
+        if(counter == 16){
+            counter = 0;
+            temp == (avg>>4);
+            avg = 0;
+            if(temp <= 200) PORTD = 0x1;
+            else if(temp <= 400) PORTD = 0x2;
+            else if(temp <= 600) PORTD = 0x4;
+            else if(temp <= 800) PORTD = 0x8;
+            else PORTD = 0x10;      
+        }        
+    }
+        
+    return 0;
+}
