4e32eac03639deb664870c06b96b1fa521fbd51d
sewingmachine18 <131910560+sewingmachine18@users.noreply.github.com>
2025-10-28 10:30:09 +0200

    Update adc_avg(3_2).c

--- exercise3/adc_avg(3_2).c
+++ exercise3/adc_avg(3_2).c
@@ -1,13 +1,16 @@
 
+
 #include <stdio.h> 
 #include <stdlib.h>
+#define F_CPU 16000000UL
 #include<avr/io.h>
 #include<avr/interrupt.h>
 #include<util/delay.h>
-#define F_CPU 16000000UL
 
 
-int dc_value, step, counter = 0, avg = 0, temp;
+
+int dc_value, step, counter = 0, temp;
+long avg = 0;
 uint16_t input;
 
 uint16_t read_adc(){
@@ -19,10 +22,7 @@ uint16_t read_adc(){
     while (ADCSRA & (1 << ADSC));
     
     //read results
-    uint8_t low  = ADCL;
-    uint8_t high = ADCH;
-    uint16_t res = (high<<8)|low;
-    return res;
+    return ADC;
 }
 
 
@@ -36,6 +36,7 @@ int main(){
     //set up ports
     DDRB = 0x2;
     DDRD = 0xFF;
+    DDRC = 0xFF;
     
     //create table and initialize duty cycle
     int pwd[] = {5, 20, 36, 51, 67, 82, 97, 113, 128, 143, 159, 174, 189, 205, 220, 236, 251};
@@ -50,18 +51,21 @@ int main(){
     
     while(1){
         
+        PORTC = step;
         //check for brightness increase(PB4)
-        if((PINB & (1<<PB4)) != 0)
+        if((PINB & 16) == 0)
             if(step < 16){
                 dc_value = pwd[++step];
                 OCR1AL = dc_value;
+                _delay_ms(200);
             }
         
         //check for brightness decrease(PB5)
-        if((PINB & 1<<PB5))
+        if((PINB & 8) == 0)
             if(step > 0){
                 dc_value = pwd[--step];
                 OCR1AL = dc_value;
+                _delay_ms(200);
             }
         
         //delay and read from adc input
@@ -73,13 +77,13 @@ int main(){
         //after 16 loops show avg through portd
         if(counter == 16){
             counter = 0;
-            temp == (avg>>4);
+            temp = (avg>>4);
+            if(temp <= 200 && temp > 0) PORTD = 0x1;
+            if(temp <= 400 && temp > 200) PORTD = 0x2;
+            if(temp <= 600 && temp > 400) PORTD = 0x4;
+            if(temp <= 800 && temp > 600) PORTD = 0x8;
+            if(temp > 800) PORTD = 0x10;    
             avg = 0;
-            if(temp <= 200) PORTD = 0x1;
-            else if(temp <= 400) PORTD = 0x2;
-            else if(temp <= 600) PORTD = 0x4;
-            else if(temp <= 800) PORTD = 0x8;
-            else PORTD = 0x10;      
         }        
     }
         
