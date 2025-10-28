0c8b12313f965ca52ba1ce536069ee2b232965c0
sewingmachine18 <131910560+sewingmachine18@users.noreply.github.com>
2025-10-28 10:30:43 +0200

    Refactor ADC and input handling in double dimmer
    Updated ADC setup and input handling for modes.

--- exercise3/double_dimmer(3_3).c
+++ exercise3/double_dimmer(3_3).c
@@ -20,43 +20,48 @@ int main(){
     //set up ports
     DDRB = 0x2;
     DDRD = 0x0;
+    DDRC = 0xFF;
     
     //create table and initialize duty cycle
     int pwd[] = {5, 20, 36, 51, 67, 82, 97, 113, 128, 143, 159, 174, 189, 205, 220, 236, 251};
     step = 8;
+    PORTC = step;
     dc_value = pwd[step];
     OCR1AL = dc_value;
     OCR1AH = 0;
     
     //set up ADC-related registers
-    ADMUX = (1<<REFS0);
+    ADMUX = (1<<REFS0)|(1<<ADLAR);
     ADCSRA = (1<<ADEN)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0);
     
     //wait for a mode to be selected
-    while( ((PIND & (1<<PD1)) == 0) && ((PIND & (1<<PD0)) == 0) );
+    while( ((PIND & 0x2) != 0) && ((PIND & 0x1) != 0) );
         
     while(1){
         
         //mode 1
-        while((PIND & (1<<PD1)) == 0){ 
+        while((PIND & 0x2) != 0){ 
             
+            PORTC = step;
             //check for brightness increase(PB4)
-            if((PINB & (1<<PB4)) != 0)
+            if((PINB & 16) == 0)
                 if(step < 16){
                     dc_value = pwd[++step];
                     OCR1AL = dc_value;
+                    _delay_ms(200);
                 }
 
             //check for brightness decrease(PB5)
-            if((PINB & 1<<PB5))
+            if((PINB & 8) == 0)
                 if(step > 0){
                     dc_value = pwd[--step];
                     OCR1AL = dc_value;
+                    _delay_ms(200);
                 }
         }
         
         //mode 2
-        while((PIND & (1<<PD0)) == 0){
+        while((PIND & 0x1) != 0){
             
             //start conversion
             ADCSRA |= (1 << ADSC);
